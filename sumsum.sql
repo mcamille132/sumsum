e1a5fe09128cd2797243e15d4e8cf12b8eeaf63a
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2020-11-19 22:04:48 +0100

    add the db for fashion

--- sumsum.sql
+++ sumsum.sql
@@ -62,26 +62,27 @@ INSERT INTO fashion (id, name, url, creator, nb_vote, baseline, localisation, co
 INSERT INTO philosopher (id, name, url, quote, nb_vote, movement, nationality, job, comments) VALUES (NULL, 'Jean-Jacques Rousseau', '476013_poster_l.jpg', 'Freedom is the power to choose our own chains.', '1367', 'movement of light', 'French', 'musician, philosopher', 'Rousseau is a great philosopher working for freedom');
 INSERT INTO painting (name, url, artist, nb_vote, style, nationality, comments) VALUES ('La Velata', 'https://i.ibb.co/Rvj4D0h/Raphae-l-1-Velata-800x800.jpg', 'Raphaël', '23400' , 'painting' , 'Italian' , 'It so wonderful!!!');
 
-INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Jean-Jacques Rousseau', 'https://i.postimg.cc/7PBgqZRL/9.png', 'Freedom is the power to choose our own chains.', '2743', 'The fight against injustice and ignorance.', 'French', 'Musician, philosopher', 'Rousseau is a great philosopher working for freedom.'), (NULL, '', '', NULL, NULL, NULL, NULL, NULL, NULL);
-
+INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Jean-Jacques Rousseau', 'https://i.postimg.cc/7PBgqZRL/9.png', 'Freedom is the power to choose our own chains.', '2743', 'The fight against injustice and ignorance.', 'French', 'Musician, philosopher', 'Rousseau is a great philosopher working for freedom.');
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Voltaire', 'https://i.postimg.cc/fyWjB3gG/2.png', 'The time is long enough for whoever takes advantage.', '2754', 'The denunciation of intolerance and religious fanaticism.', 'French', 'Writer and philosopher', 'Voltaire is a wonderful philosopher.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Denis Diderot', 'https://i.postimg.cc/FKgL8rcN/6.png', 'The things that are talked about most among men are quite usually the things that we know the least.\"advantage.', '1860', 'The fight against injustice and ignorance.', 'French', 'Writer and philosopher', 'Diderot is a great writer.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Montesqieu', 'https://i.postimg.cc/28sL5b7C/4.png', 'A thing is not right because it is law; but it must be law because it is just.', '2780', 'Social and political protest.', 'French', 'Mathematician, physicist, philosopher. ', 'Montesquieu is a great mathematician.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Jean Le Rond D’alembert', NULL, 'https://i.postimg.cc/8zLJfpZM/7.png', '860', 'Hostility to slavery.', 'French', 'Political thinker.', 'Jean Le Rond D’alembert is a great political thinker.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'René Descartes', 'https://i.postimg.cc/Vv50JXhk/8.png', 'I think so I am.. ', '3965', 'The fight against injustice and ignorance.', 'French', 'Mathematician, physicist and philosopher.', 'René Descartes is a great physicist.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Nicolas Machiavel', 'https://i.postimg.cc/15fDXZxr/10.png', 'Where the will is great, the difficulties diminish.', '2754', 'The fight against injustice and ignorance.', 'Italian', 'Writer and politician.', 'Nicolas Machiavel is a great speaker.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Thomas Hobbes', 'https://i.postimg.cc/TP9KQkMP/11.png', 'The privilege of absurdity is reserved for the only human creature..', '1396', 'The fight against injustice and ignorance.', 'English', 'Writer and philosopher.', 'Thomas Hobbesl is a great writer.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Nicolas de Condorcet', 'https://i.postimg.cc/j2jPypDK/5.png', 'Friends of the truth are those who seek it, not those who boast of having found it.', '1854', 'The fight against injustice and ignorance.', 'French', 'Mathematician, philosopher, politician.', 'Nicolas de Condorcet is  a great politician.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'Cesare Beccaria', 'https://i.postimg.cc/0ySmGdy6/3.png', 'The death penalty has never turned away men determined to outrage society.', '1395', 'The fight against injustice and ignorance.', 'Italian', 'Jurist, criminal lawyer, philosopher, economist', 'Nicolas de Condorcet is  a great jurist, criminal lawyer.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'John Locke', 'https://i.postimg.cc/L8XqvcB0/12.png', 'A tax loosely established offers a great deal of fraud lure.', '1858', 'The denunciation of intolerance and religious fanaticism.', 'Scotish', 'Philosopher, economist', 'John Locke is a great Scotish economist.');
-
 INSERT INTO `philosopher` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `job`, `comments`) VALUES (NULL, 'John Locke', 'https://i.postimg.cc/sXHvCv2d/Sans-titre-1-R-cup-r.png', 'Rarely do we listen to the most obvious reasons..', '1127', 'The denunciation of intolerance and religious fanaticism.', 'English', 'philosopher', 'John Locke is a great English philosopher.');
+
+INSERT INTO `fashion` (`id`, `name`, `url`, `artist`, `nb_vote`, `style`, `nationality`, `comments`) VALUES
+(1, 'Cap-hat', 'https://postimg.cc/XpVXfFvh', 'Cunegonde de Villia', 2006, 'To feel in the city as well as in the countryside', 'France', 'The explosive mix of Franco-Italian elegance'),
+(2, 'Italian Headdress', 'https://i.postimg.cc/wTR5jdLY/depressif.png', 'Angelina Bottecina', 1000, 'The must have to be the star of your event', 'Italia', 'Even better than the ruffle collar'),
+(3, 'English Headdress', 'https://i.postimg.cc/mgNHPZrJ/englishdress.png', 'Roxane of Wessex', 390, 'Old fashioned style for classic fashion outfit', 'England', 'So old-fashioned! '),
+(4, 'German Headdress', 'https://i.postimg.cc/sxhBss8n/germanhairdress.png', 'Franziska Van Grünt', 600, 'Wide brimmed hats, for wild events', 'German', 'Special effect for special events! Love it!'),
+(5, 'Ruff Colar', 'https://i.postimg.cc/FRnchPKK/ruffcolar.png', 'Elizabeth ', 4000, 'For everyone, anytime', 'England', 'I nurture a special love that borders on obsession for ruffles, just because they are so feminine and splendid. '),
+(6, 'Full Jewelry', 'https://i.postimg.cc/kDP25LP7/jewelry.png', 'Margaret of Austria', 6059, 'Wear jewelry only on days ending with « y »', 'Austria', 'Brilliant, a must have!!'),
+(7, 'Feathers fans', 'https://i.postimg.cc/NF9XF1yk/feather.png', 'Robert Henz', 2799, 'Elegance and presence in deutch', 'German', 'So fancy! I\'ve had nothing but compliments since the feather fans became my must have.'),
+(8, 'Beret pearl feather', 'https://i.postimg.cc/0NYc2qgy/1.png', 'Enrico Manchol', 1270, 'Extraordinary people definitely wears beret', 'Spanish', 'Love it!'),
+(9, 'Renaissance bustier', 'https://i.postimg.cc/kGzxvjrH/bustier.png', 'Charlotte de la Trémoille', 10000, 'Beauty for all ', 'French', 'A must... but suffocating! Looking forward to a change!'),
+(10, 'Pantyhose and Tights', 'https://i.postimg.cc/QNwnXDVc/pantyhorse.png', 'Juan de Austriano', 3780, 'Verility in its purest form', 'Austria', 'I LOVE IT! Thank you for this fashion innovation');
