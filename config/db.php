e62cf4ba4cf4435045093f5e16c02b760cb984ea
Thomas Paoli <34407263+tyZulh@users.noreply.github.com>
2020-12-09 20:45:36 +0100

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file define BD access infos.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   PHP_SimpleMVC
+ * @author    WCS <contact@wildcodeschool.fr>
+ * @license  --
+ * @link     https://github.com/WildCodeSchool/simple-mvc
+ *
+ */
+define('APP_DB_USER', 'root');
+define('APP_DB_PWD', 'Qwerty2020!');
+define('APP_DB_HOST', 'srv-captain--citizen-court-db');
+define('APP_DB_NAME', 'sumsum');
